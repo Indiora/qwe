cfcb187a4d64f5b5f7d80c8399379af607d1c7cc
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-21 16:29:07 +0300

    Add files via upload

---
+++ asd/abs_min.h
@@ -0,0 +1,13 @@
+#include "abs_min.h"
+int abs_min(int array[], int size)
+{
+    int min = array[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (abs(array[i]) < abs(min))
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
