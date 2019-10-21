45bbbe5a6225073d02b669b74e3766d074659e65
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-21 16:24:35 +0300

    Add files via upload

---
+++ asd/abs_max.h
@@ -0,0 +1,13 @@
+#include "abs_max.h"
+int abs_max(int array[], int size)
+{
+    int max = array[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (abs(array[i]) > abs(max))
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
