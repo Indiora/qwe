7e610223477e0aa3993f417730ca1cf9c94c0a1e
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-21 16:35:35 +0300

    Add files via upload

---
+++ asd/sum.h
@@ -0,0 +1,15 @@
+#include "sum.h"
+int sum(int array[], int size)
+{
+    int sum = 0;
+    int flag = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if ((flag == 1) || (array[i] == abs_max(array,size)))
+        {
+            sum = sum + array[i];
+            flag = 1;
+        }
+    }
+    return sum;
+}
