4a5873b2b1391325647510ebe21279bef9487175
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-23 14:32:16 +0300

    Update abs_max.c

--- asd/abs_max.c
+++ asd/abs_max.c
@@ -1,9 +1,9 @@
 #include "abs_max.h"
 int abs_max(int array[], int size)
 {
-    int i=0;
+    int i;
     int max = array[0];
-    for (int i; i < size; i++)
+    for (i=0; i < size; i++)
     {
         if (abs(array[i]) > abs(max))
         {
