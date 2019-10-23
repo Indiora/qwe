5cf39fe6437fbcc20d25685df90ffde36f2c69bd
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-23 14:37:16 +0300

    Update menu.c

--- asd/menu.c
+++ asd/menu.c
@@ -5,56 +5,6 @@
 #include "diff.h"
 #include "sum.h"
 
-int abs_max(int array[], int size)
-{
-    int max = array[0];
-    for (int i=0; i < size; i++)
-    {
-        if (abs(array[i]) > abs(max))
-        {
-            max = array[i];
-        }
-    }
-    return max;
-}
-
-
-int abs_min(int array[], int size)
-{
-    int min = array[0];
-    for (int i=0; i < size; i++)
-    {
-        if (abs(array[i]) < abs(min))
-        {
-            min = array[i];
-        }
-    }
-    return min;
-}
-
-
-int diff(int array[], int size)
-{
-    return abs_max(array, size) - abs_min(array, size);
-}
-
-
-int sum(int array[], int size)
-{
-    int sum = 0;
-    int flag = 0;
-    for (int i=0; i < size; i++)
-    {
-        if ((flag == 1) || (array[i] == abs_max(array,size)))
-        {
-            sum = sum + array[i];
-            flag = 1;
-        }
-    }
-    return sum;
-}
-
-
 int main()
 {
     int array[100];
