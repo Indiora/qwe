e416cd82504b17383955f368990978a66834d4a4
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-23 14:27:10 +0300

    Update sum.c

--- asd/sum.c
+++ asd/sum.c
@@ -1,9 +1,10 @@
 #include "sum.h"
 int sum(int array[], int size)
 {
+    int i=0;
     int sum = 0;
     int flag = 0;
-    for (int i = 0; i < size; i++)
+    for (int i; i < size; i++)
     {
         if ((flag == 1) || (array[i] == abs_max(array,size)))
         {
