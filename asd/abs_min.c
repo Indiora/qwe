aa305e19d91bb9f09b2c6eb0f3baa91a770cd6db
Indiora <54910571+Indiora@users.noreply.github.com>
2019-10-25 18:52:30 +0300

    Update abs_min.c

--- asd/abs_min.c
+++ asd/abs_min.c
@@ -1,14 +1,13 @@
-#include "abs_min.h"
-int abs_min(int array[], int size)
+#include <stdlib.h>
+int abs_min(int array[], int count)
 {
-    int min = array[0];
-    int i = 0;
-    for (i=0; i < size; i++)
-    {
-        if (abs(array[i]) < abs(min))
-        {
-            min = array[i];
-        }
-    }
-    return min;
+int min = array[0];
+for (int i = 0; i < count; i++)
+{
+if (abs(array[i]) < abs(min))
+{
+min = array[i];
+}
+}
+return min;
 }
